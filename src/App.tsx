d7d3468cd99ded339f0b989de0bac916d8df9721
ferrantealberto <143766026+ferrantealberto@users.noreply.github.com>
2025-05-31 18:07:08 +0200

    Simplify MUI implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import { ThemeProvider, CssBaseline } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
-import { theme } from './theme';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </ThemeProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
